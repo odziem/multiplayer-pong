3528dbba54df3cb5aa7e28a05941bea1cd3678fd
Adam Odziemkowski <odziem@users.noreply.github.com>
2020-10-02 04:57:13 -0400

    Listen server

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ const PORT = 3000;
 server.listen(PORT);
 console.log(`Listening on port ${PORT}...`);
 
+let readyPlayerCount = 0;
+
 io.on('connection', (socket) => {
   console.log('a user connected', socket.id);
+
+  socket.on('ready', () => {
+    console.log('Player ready', socket.id);
+
+    readyPlayerCount++;
+
+    if (readyPlayerCount === 2) {
+      //broadcast('startGame')
+    }
+  });
 });
