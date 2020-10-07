5af91bbb593377427dc0b7bb96f5b9ab4db4bb69
Adam Odziemkowski <odziem@users.noreply.github.com>
2020-10-07 03:13:20 -0400

    Disconnect handling

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ io.on('connection', (socket) => {
 
     readyPlayerCount++;
 
-    if (readyPlayerCount === 2) {
+    if (readyPlayerCount % 2 === 0) {
       io.emit('startGame', socket.id);
     }
   });
@@ -28,4 +28,8 @@ io.on('connection', (socket) => {
   socket.on('ballMove', (ballData) => {
     socket.broadcast.emit('ballMove', ballData);
   });
+
+  socket.on('disconnect', (reason) => {
+    console.log(`Client ${socket.id} disconnected: ${reason}`);
+  });   
 });
