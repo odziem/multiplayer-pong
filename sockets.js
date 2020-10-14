020f29867892476409bdd04511159b4b9741c77e
Adam Odziemkowski <odziem@users.noreply.github.com>
2020-10-14 00:43:17 -0400

    Rooms

--- sockets.js
+++ sockets.js
@@ -3,28 +3,34 @@ let readyPlayerCount = 0;
 function listen(io) {
   const pongNamespace = io.of('/pong');
   pongNamespace.on('connection', (socket) => {
+    let room;
+
     console.log('a user connected', socket.id);
 
     socket.on('ready', () => {
-      console.log('Player ready', socket.id);
+      room = 'room' + Math.floor(readyPlayerCount / 2);
+      socket.join(room);
+
+      console.log('Player ready', socket.id, room);
 
       readyPlayerCount++;
 
       if (readyPlayerCount % 2 === 0) {
-        pongNamespace.emit('startGame', socket.id);
+        pongNamespace.in(room).emit('startGame', socket.id);
       }
     });
 
     socket.on('paddleMove', (paddleData) => {
-      socket.broadcast.emit('paddleMove', paddleData);
+      socket.to(room).emit('paddleMove', paddleData);
     });
 
     socket.on('ballMove', (ballData) => {
-      socket.broadcast.emit('ballMove', ballData);
+      socket.to(room).emit('ballMove', ballData);
     });
 
     socket.on('disconnect', (reason) => {
       console.log(`Client ${socket.id} disconnected: ${reason}`);
+      socket.leave(room);
     });   
   })
 }
